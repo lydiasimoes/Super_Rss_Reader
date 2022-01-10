b28253ee618b26d8991fa4dc2bda16eebf6f9aaa
garygaffez <gary.gaffez@gmail.com>
2022-01-10 14:03:06 +0100

    home-checkbox

--- index.php
+++ index.php
@@ -5,8 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Super Rss Reader</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
-<body>
+<body class="bg-secondary">
+
+<h1 class="h2 text-center text-white mb-5 mt-3">Veuillez sélectionner 3 sujets parmi les 5 suivants</h1>
+
+<div class="container p-0">
+    <div class="row justify-content-center">
+        <div class="col col-sm-5">
+            <div class="list-group">
+                <label class="list-group-item mb-3">
+                    <input class="form-check-input me-3" type="checkbox" value="1">
+                    Football
+                </label>
+                <label class="list-group-item mb-3">
+                    <input class="form-check-input me-3" type="checkbox" value="2">
+                    Rugby
+                </label>
+                <label class="list-group-item mb-3">
+                    <input class="form-check-input me-3" type="checkbox" value="3">
+                    Tennis
+                </label>
+                <label class="list-group-item mb-3">
+                    <input class="form-check-input me-3" type="checkbox" value="4">
+                    Basket
+                </label>
+                <label class="list-group-item mb-3">
+                    <input class="form-check-input me-3" type="checkbox" value="5">
+                    Handball
+                </label>
+            </div>
+        </div> 
+    <div class="row justify-content-center">
+        <div class="d-flex col-6 col-sm-6 justify-content-center">
+            <button type="submit" class="btn btn-primary btn-lg mt-5">Valider</button>
+        </div>
+    </div>       
+    </div>    
+</div>
+
+
     
+
 </body>
 </html>
