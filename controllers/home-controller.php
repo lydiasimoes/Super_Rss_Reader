2d4ef2636351202acdecc080937f8213ac0fa40c
levequemathieu <mathieuleveque2@gmail.com>
2022-01-10 14:05:14 +0100

    ok

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -1,6 +1,18 @@
 <?php 
 
+include(dirname(__FILE__). '/../views/home.php');
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $lastname = trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+        $lastname = trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+        $lastname = trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+        $lastname = trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));
+        $lastname = trim(filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES));                                                                           
+        
+    }
 
-
-
+    if(!($_SERVER["REQUEST_METHOD"] == "POST") || !empty($Array)){
+        include(dirname(__FILE__). '/../views/home.php');
+    }else{
+        include(dirname(__FILE__). '/../views/pages.php');
+    }
 
