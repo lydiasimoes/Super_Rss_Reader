6693ec3b377c302fce1c56a46c3e89afe8ecd927
garygaffez <gary.gaffez@gmail.com>
2022-01-10 16:56:48 +0100

    fin parametres checkbox

--- views/parameters.php
+++ views/parameters.php
@@ -1,3 +1,25 @@
+<?php
+
+$errorArray = [];
+
+$screenMode = intval(filter_input(INPUT_POST,'screenMode',FILTER_SANITIZE_NUMBER_INT));
+if ($screenMode < 1 || $screenMode > 2) {
+    $errorArray['errorscreenMode'] = 'c\'est interdit';
+}
+
+$articleNumber = intval(filter_input(INPUT_POST,'articleNumber',FILTER_SANITIZE_NUMBER_INT));
+if ($articleNumber < 1 || $articleNumber > 2) {
+    $errorArray['errorarticleNumber'] = 'c\'est interdit';
+} 
+
+$subject = intval(filter_input(INPUT_POST,'subject',FILTER_SANITIZE_NUMBER_INT));
+if ($subject < 3 || $subject > 3) {
+    $errorArray['errorsubject'] = 'Sélectionnez 3 articles !';
+} 
+
+var_dump($errorArray);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,47 +30,78 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
+<body class="bg-secondary">
 
-<div class="container bg-secondary p-0">
+<div class="container p-0">
     <div class="row justify-content-center">
-        <div class="col col-sm-8">
-            <div class="list-group">
-                <label class="list-group-item">Mode de l'écran :
-                    <input class="form-check-input me-1" type="radio" value="1">
-                    Light
-                    <input class="form-check-input me-1" type="radio" value="2">
-                    Dark
-                </label>
-                <label class="list-group-item">Nombre d'articles : 
-                    <input class="form-check-input me-1" type="radio" value="3">
-                    6
-                </label>
-                <label class="list-group-item">
-                    <input class="form-check-input me-1" type="radio" value="4">
-                    9
-                </label>
-                <label class="list-group-item">
-                    <input class="form-check-input me-1" type="radio" value="5">
-                    12
-                </label>
+        <div class="col col-lg-8">
 
-                <label class="list-group-item">Mode de l'écran :
-                    <input class="form-check-input me-1" type="checkbox" value="1">
-                    Football
-                    <input class="form-check-input me-1" type="checkbox" value="2">
-                    Rugby
-                    <input class="form-check-input me-1" type="checkbox" value="3">
-                    Tennis
-                    <input class="form-check-input me-1" type="checkbox" value="4">
-                    Basket
-                    <input class="form-check-input me-1" type="checkbox" value="5">
-                    Handball
-                </label>
+        <form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST" novalidate class="d-flex flex-column justify-content-center">
+            <div class="container bg-light">
+                <div class="row justify-content-center">
+                <p class="mb-2 mt-3 text-center fw-bold">Mode de l'écran : </p><br> 
+                    <div class="col d-flex justify-content-center p-2">
+                        
+                        <input type="radio"  id="light" name="screenMode" value="1" required>
+                        <label for="light" class="ms-2 me-5">Light</label>
+                            
+                        <input type="radio" id="dark" name="screenMode" value="2" required>
+                        <label for="dark" class="ms-2">Dark</label>
+                        
+                    </div>                   
+                </div>
+                
+                <div class="row">
+                    <p class="mb-2 mt-3 text-center fw-bold">Nombre d'articles :</p>
+                    <div class="col d-flex justify-content-center p-2">
+                        
+                        <input type="radio" id="six" name="articleNumber" value="1" required>
+                        <label for="six" class="ms-2 me-5">6</label>
+                            
+                        <input type="radio" id="nine" name="articleNumber" value="2" required>
+                        <label for="nine"class="ms-2 me-5">9</label>
+
+                        <input type="radio" id="twelve" name="articleNumber" value="2" required>
+                        <label for="twelve"class="ms-2">12</label>
+                    </div>
+                
+                <div class="row">
+                    <p class="mb-2 mt-3 text-center fw-bold">Sélectionnez vos sujets :</p>  
+                    <div class="col d-flex flex-column align-items-center p-2">
+                        <div>
+                            <input type="checkbox" id="football" name="subject" value="1" required>
+                            <label for="football" class="ms-2">Football</label>
+                        </div>        
+                        
+                        <div>
+                            <input type="checkbox" id="tennis" name="subject" value="2" required>
+                            <label for="tennis"class="ms-2">Tennis</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="rugby" name="subject" value="3" required>
+                            <label for="rugby"class="ms-2">Rugby</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="basket" name="subject" value="4" required>
+                            <label for="basket"class="ms-2">Basket</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="handball" name="subject" value="5" required>
+                            <label for="handball"class="ms-2">Handball</label> 
+                        </div>  
+                        <div class="text-dark fst-italic mb-3 mt-3">
+                            <?=$errorArray['errorsubject'] ?? '';?>
+                        </div>                        
+                    </div>
+                </div>                
             </div>
-        </div>
-    </div>    
-</div>
+            
+            <div class="d-flex justify-content-center">
+                <button type="submit" id="inputValid" class="btn btn-primary mt-4 mb-3 fw-bold rounded-pill w-25">GO !</button>
+            </div>
+        </form>                                      
+    </div>                
+</div>            
     
 
 </body>
