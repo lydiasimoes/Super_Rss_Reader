39c3630386a4314333c592c798951c51f344453f
Lydia Simoes <lydia_80@hotmail.fr>
2022-01-10 14:46:51 +0100

    header et footer

--- views/home.php
+++ views/home.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>Super Rss Reader</title>
-</head>
 
-<body class="bg-secondary">
-
-<!-- ***** navbar **** -->
+    <?php include(dirname(__FILE__).'../../header.php');?>
+            <!-- ***** navbar **** -->
     <?php include(dirname(__FILE__).'/template/navbar.php');?>
 
 <h1 class="h2 text-center text-white mb-5 mt-3">Veuillez sélectionner 3 sujets parmi les 5 catégories suivantes</h1>
