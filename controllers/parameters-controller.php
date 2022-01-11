75683474a74392f7c75451258af6374c607eb3b6
garygaffez <gary.gaffez@gmail.com>
2022-01-11 12:03:30 +0100

    suite parameter cookie

--- controllers/parameters-controller.php
+++ controllers/parameters-controller.php
@@ -1,3 +1,41 @@
 <?php
+
+$errorArray = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$screenMode = intval(filter_input(INPUT_POST,'screenMode',FILTER_SANITIZE_NUMBER_INT));
+if ($screenMode < 1 || $screenMode > 1) {
+    $errorArray['errorscreenMode'] = 'c\'est interdit';
+}
+
+$articleNumber = intval(filter_input(INPUT_POST,'articleNumber',FILTER_SANITIZE_NUMBER_INT));
+if ($articleNumber < 1 || $articleNumber > 2) {
+    $errorArray['errorarticleNumber'] = 'c\'est interdit';
+} 
+
+var_dump($_POST['subject']);
+$subject = filter_input(INPUT_POST,'subject',FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+var_dump($subject);
+foreach ($subject as $key => $value) {
+    if ($value < 1 || $value > 5) {
+        $errorArray['errorValue'] = 'c\'est interdit';
+    } 
+}
+
+setcookie("cookieSport", json_encode($subject), time()+60*60*24*7);
+
+}
+
     include(dirname(__FILE__).'/../views/parameters.php');
-?>
+
+    if (($_SERVER["REQUEST_METHOD"] != "POST") || !empty($errorArray)) { 
+
+        include(dirname(__FILE__).'/../views/form.php');
+
+    } else { 
+
+
+        include(dirname(__FILE__).'/../views/pages.php');
+        
+        }
--- views/parameters.php
+++ views/parameters.php
@@ -1,24 +1,3 @@
-<?php
-
-$errorArray = [];
-
-$screenMode = intval(filter_input(INPUT_POST,'screenMode',FILTER_SANITIZE_NUMBER_INT));
-if ($screenMode < 1 || $screenMode > 2) {
-    $errorArray['errorscreenMode'] = 'c\'est interdit';
-}
-
-$articleNumber = intval(filter_input(INPUT_POST,'articleNumber',FILTER_SANITIZE_NUMBER_INT));
-if ($articleNumber < 1 || $articleNumber > 2) {
-    $errorArray['errorarticleNumber'] = 'c\'est interdit';
-} 
-
-$subject = intval(filter_input(INPUT_POST,'subject',FILTER_SANITIZE_NUMBER_INT));
-if ($subject < 3 || $subject > 3) {
-    $errorArray['errorsubject'] = 'Sélectionnez 3 articles !';
-} 
-
-var_dump($errorArray);
-?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -69,35 +48,37 @@ var_dump($errorArray);
                     <p class="mb-2 mt-3 text-center fw-bold">Sélectionnez vos sujets :</p>  
                     <div class="col d-flex flex-column align-items-center p-2">
                         <div>
-                            <input type="checkbox" id="football" name="subject" value="1" required>
+                            <input type="checkbox" id="football" name="subject[]" value="1" required>
                             <label for="football" class="ms-2">Football</label>
                         </div>        
                         
                         <div>
-                            <input type="checkbox" id="tennis" name="subject" value="2" required>
+                            <input type="checkbox" id="tennis" name="subject[]" value="2" required>
                             <label for="tennis"class="ms-2">Tennis</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="rugby" name="subject" value="3" required>
+                            <input type="checkbox" id="rugby" name="subject[]" value="3" required>
                             <label for="rugby"class="ms-2">Rugby</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="basket" name="subject" value="4" required>
+                            <input type="checkbox" id="basket" name="subject[]" value="4" required>
                             <label for="basket"class="ms-2">Basket</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="handball" name="subject" value="5" required>
+                            <input type="checkbox" id="handball" name="subject[]" value="5" required>
                             <label for="handball"class="ms-2">Handball</label> 
                         </div>  
                         <div class="text-dark fst-italic mb-3 mt-3">
                             <?=$errorArray['errorsubject'] ?? '';?>
-                        </div>                        
+                        </div>  
+                        
+                        
                     </div>
                 </div>                
             </div>
             
             <div class="d-flex justify-content-center">
-                <button type="submit" id="inputValid" class="btn btn-primary mt-4 mb-3 fw-bold rounded-pill w-25">GO !</button>
+                <button type="submit" name="valider" id="inputValid" class="btn btn-primary mt-4 mb-3 fw-bold rounded-pill w-25">GO !</button>
             </div>
         </form>                                      
     </div>                
